e6f78937f11cd1a21e3acd18b7d362f78344af8e
amirmoumen <amirmoumen06@yahoo.fr>
2022-02-27 17:07:18 -0800

    firebase auth done + firestore db set up

--- app/src/main/java/com/zayox/skypeclone/MainActivity.java
+++ app/src/main/java/com/zayox/skypeclone/MainActivity.java
@@ -1,14 +1,67 @@
 package com.zayox.skypeclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editEmail, editPassword;
+    private Button loginBtn;
+    private TextView registerLink;
+    private FirebaseAuth auth;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editEmail = findViewById(R.id.email_login);
+        editPassword = findViewById(R.id.password_login);
+        loginBtn = findViewById(R.id.login_btn);
+        registerLink = findViewById(R.id.register_link);
+
+        auth = FirebaseAuth.getInstance();
+
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String email = editEmail.getText().toString();
+                String password = editPassword.getText().toString();
+
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+                            finish();
+                        }
+                    }
+                });
+            }
+        });
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/zayox/skypeclone/RegistrationActivity.java
+++ app/src/main/java/com/zayox/skypeclone/RegistrationActivity.java
@@ -1,19 +1,73 @@
 package com.zayox.skypeclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private EditText editTextName, editTextEmail, editTextPassword;
     private Button buttonRegister;
+    private TextView loginLink;
+    private FirebaseAuth auth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        editTextName = findViewById(R.id.name_register);
+        editTextEmail =  findViewById(R.id.email_register);
+        editTextPassword =  findViewById(R.id.password_register);
+        buttonRegister = findViewById(R.id.register_btn);
+        loginLink = findViewById(R.id.login_link);
+
+        auth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registration in progress...");
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog.show();
+                String name = editTextName.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            Toast.makeText(RegistrationActivity.this, "Registration successful", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+                        }
+                    }
+                });
+            }
+        });
+
+        loginLink.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+            }
+        });
+
+
     }
 }
