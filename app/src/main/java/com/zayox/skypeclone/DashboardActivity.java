74f28445e5258946e366b558d20397bfdf959caa
Zayox <amirmoumen06@yahoo.fr>
2022-03-27 21:53:20 +0200

    jitsi sdk implemented

--- app/src/main/java/com/zayox/skypeclone/DashboardActivity.java
+++ app/src/main/java/com/zayox/skypeclone/DashboardActivity.java
@@ -10,6 +10,9 @@ import android.widget.EditText;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
 
 
 public class DashboardActivity extends AppCompatActivity {
@@ -30,6 +33,11 @@ public class DashboardActivity extends AppCompatActivity {
 
         try {
             serverURL = new URL("https://meet.jit.si");
+            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                    .setServerURL(serverURL)
+                    .build();
+            JitsiMeet.setDefaultConferenceOptions(options);
+
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -38,7 +46,10 @@ public class DashboardActivity extends AppCompatActivity {
         codeValidation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                    JitsiMeetConferenceOptions m_options = new JitsiMeetConferenceOptions.Builder()
+                            .setRoom(codeInput.getText().toString())
+                            .build();
+                JitsiMeetActivity.launch(DashboardActivity.this, m_options);
             }
         });
 
